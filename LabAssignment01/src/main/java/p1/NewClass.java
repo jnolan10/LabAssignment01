c018646747966da49853ecffa0c64d038b0e909d
jonat <jonat@137.125.155.241>
2021-09-09 11:06:09 -0400

    My first lab

---
+++ LabAssignment01/src/main/java/p1/NewClass.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class NewClass {
+    public static void main(String[] args) {
+        //loop increments to 100 
+        for(int i = 0; i <= 100; i++)
+        {
+            //if the incrementing variable is even enter the statement body
+            if((i % 2) == 0)
+            {
+                //print the incrementing variable on its own line
+                System.out.println(i);
+            }
+        }
+    }
+}
